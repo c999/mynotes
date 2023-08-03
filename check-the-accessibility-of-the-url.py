4914215d488e9fb44df9a400c6822c6100f495e5
Wind <42412083+c999@users.noreply.github.com>
2023-08-03 13:38:05 +0800

    Create check-the-accessibility-of-the-url.py

---
+++ check-the-accessibility-of-the-url.py
@@ -0,0 +1,29 @@
+import itertools
+import requests
+
+def generate_combinations(string_to_concat):
+    letters = 'abcdefghijklmnopqrstuvwxyz'
+    combinations = list(itertools.combinations(letters, 3))
+    results = []
+
+    for combination in combinations:
+        result = string_to_concat + ''.join(combination)
+        results.append(result)
+
+    return results
+
+def check_urls(urls, output_file):
+    with open(output_file, "w") as file:
+        for url in urls:
+            response = requests.get(url)
+            if response.status_code == 404:
+                file.write(f"{url} returned a 404 status code.\n")
+
+# Generate combinations
+string_to_concat = "http://github.com/"
+combinations = generate_combinations(string_to_concat)
+
+# Check URLs for 404 status code and save output to a file
+output_file = "output.txt"
+check_urls(combinations, output_file)
+
